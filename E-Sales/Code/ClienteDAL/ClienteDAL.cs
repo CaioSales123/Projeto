0c61ab7b5a3fed85eaca73f85222ffc35cf97cdc
Yuri Volkov <40499768+yurivolkov@users.noreply.github.com>
2018-06-26 14:52:21 -0300

    Arrumando as classes

--- E-Sales/Code/ClienteDAL/ClienteDAL.cs
+++ E-Sales/Code/ClienteDAL/ClienteDAL.cs
@@ -4,10 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DAL
+namespace E_Sales.Code.ClienteDAL
 {
-    public class ClienteDAL
+    class ClienteDAL
     {
-
     }
 }
